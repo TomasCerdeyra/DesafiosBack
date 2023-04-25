ab1f2c8d40358e30577f61450ca432a1eb558b4a
TomasCerdeyra <tomascerdeyraimport@gmail.com>
2023-04-25 18:04:29 -0300

    Delete Desafio42 directory

--- Desafio42/src/api/middlewares/redirectMiddleware.js
+++
@@ -1,9 +0,0 @@
-const redirectMiddleware = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next()
-    } else {
-        res.redirect('/pages/login.html')
-    }
-}
-
-module.exports = redirectMiddleware
